bb72974f3035748a79b4b663b3c1676eb924e7c6
dmp42 <olivier@webitup.fr>
2011-11-16 22:38:49 +0100

    No more "deploy" operations

--- pukefile.py
+++ pukefile.py
@@ -21,68 +21,67 @@ r.merge('puke-' + PLATFORM + ".yaml")
 r.yak('invariant')
 r.yak('platform')
 
+
+global FILTERING
+FILTERING="*-stable.js"
+
+# Just make-deploy by default
 @task("Default task called")
 def default():
+    executeTask("build")
     executeTask("deploy")
-
-@task("All tasks called")
-def all():
-    executeTask("deploy")
-#    executeTask("flintdeploy")
-    executeTask("mintdeploy")
     executeTask("stats")
 
+# Clean build and dist
 @task("Washing-up the taupe :)")
 def clean():
     rm(Yak.BUILD_ROOT)
     rm(Yak.DEPLOY_ROOT)
 
+# Get whatever has been built and exfilter some crappy stuff
 @task("Deploy")
 def deploy():
-    executeTask("build")
     list = FileList(Yak.BUILD_ROOT, exclude = "*.zip,*.tar.gz,*.DS_Store")
     deepcopy(list, Yak.DEPLOY_ROOT)
 
+# Build, minify, deploy, stats
+@task("All tasks called")
+def all():
+    executeTask("build")
+#    executeTask("flint")
+    executeTask("mint")
+    executeTask("deploy")
+    executeTask("stats")
+
 @task("Stats report deploy")
 def stats():
-    list = FileList(Yak.DEPLOY_ROOT, filter = "*-stable.js")
-    (files, lines, size) = stats(list, title = "Static statistics - javascript")
-#    console.log(files, lines, size)
-
-#    list = FileList(Yak.DEPLOY_ROOT, filter = "*-stable.js.gz")
-#    stats(list, title = "Static statistics - javascript (+gzed)")
-
-    list = FileList(Yak.DEPLOY_ROOT, filter = "*-stable-min.js")
+    list = FileList(Yak.BUILD_ROOT, filter = FILTERING)
+    stats(list, title = "Static statistics - javascript")
+    list = FileList(Yak.BUILD_ROOT, filter = FILTERING.replace(".js", "-min.js"))
     stats(list, title = "Static statistics - minified javascript")
-
-#    list = FileList(Yak.DEPLOY_ROOT, filter = "*-stable-min.js.gz")
-#    stats(list, title = "Static statistics - minified javascript (+gzed)")
-
-    list = FileList(Yak.DEPLOY_ROOT, filter = "*-stable.css")
+    list = FileList(Yak.BUILD_ROOT, filter = "*-stable.css")
     stats(list, title = "Static statistics - css")
     list = FileList(Yak.DEPLOY_ROOT, filter = "*.html,*.xml,*.txt")
     stats(list, title = "Static statistics - (ht|x)ml + txt")
-    list = FileList(Yak.DEPLOY_ROOT, exclude = "*.html,*.xml,*.txt,*.js,*.css")
+    list = FileList(Yak.BUILD_ROOT, exclude = "*.html,*.xml,*.txt,*.js,*.css")
     stats(list, title = "Static statistics - other")
 
 @task("Linting")
-def lintdeploy():
-    list = FileList(Yak.DEPLOY_ROOT, filter = "*-stable.js")
+def lint():
+    list = FileList(Yak.BUILD_ROOT, filter = FILTERING)
     jslint(list, strict=False, nojsdoc=True, relax=True)
 
 @task("Flint")
-def flintdeploy():
-    list = FileList(Yak.DEPLOY_ROOT, filter = "*-stable.js")
+def flint():
+    list = FileList(Yak.BUILD_ROOT, filter = FILTERING)
     jslint(list, strict=False, nojsdoc=True, relax=True, fix=True)
 
 @task("Minting")
-def mintdeploy():
-    list = FileList(Yak.DEPLOY_ROOT, filter = "*-stable.js")
+def mint():
+    list = FileList(Yak.BUILD_ROOT, filter = FILTERING)
     for burne in list.get():
-        m = burne.replace("-stable.js", "-stable-min.js")
+        m = burne.replace(".js", "-min.js")
         minify(burne, m)
-#        pack(burne, burne + ".gz")
-#        pack(m, m + ".gz")
 
 @task("Deploying the static ressources, including approved third party dependencies")
 def build():
@@ -114,7 +113,6 @@ def build():
                 description += "\t\t<dt>" + key + "</dt>\n\t\t<dd>" + "</dd><dd>".join(v) + "</dd>\n"
         description += "\t</dl>\n</section>\n"
         deepcopy(burne["Source"], os.path.join(Yak.BUILD_ROOT, "lib", burne["Destination"]))
-        sh("cd " + Yak.BUILD_ROOT + "/lib/" + burne["Destination"] + "; rm " + k + "; ln -s " + burne["Latest"] + " " +  k )
 
     # h5 = os.path.join(BUILD_ROOT, "lib", fulllist["h5bp-latest"]["Destination"])
     # sh("cd " + h5 + "; rm builder.zip; mv builder* builder.zip")
@@ -131,6 +129,11 @@ def build():
     labjs = os.path.join(Yak.BUILD_ROOT, "lib", Yak.COLLECTION["labjs-stable.js"]["Destination"])
     unpack(os.path.join(labjs, "LABjs-2.0.3.zip"), os.path.join(labjs, "2.0.3"))
 
+    for (k, burne) in Yak.COLLECTION.items():
+#        sh("cd " + Yak.BUILD_ROOT + "/lib/" + burne["Destination"] + "; rm " + k + "; ln -s " + burne["Latest"] + " " +  k )
+        sh("cd " + Yak.BUILD_ROOT + "/lib/" + burne["Destination"] + "; cp -R " + burne["Latest"] + " " +  k )
+
+
     # Build-up the description file
     file = "src/doc.html"
     s = Sed()
@@ -138,6 +141,6 @@ def build():
     deepcopy(file, Yak.BUILD_ROOT, replace=s)
 
     # Deepcopy other stuff
-    list = FileList("src", exclude="*robots.txt,*crossdomain.xml,*doc.html")
+    list = FileList("src", exclude="*robots.txt,*crossdomain.xml,*doc.html,*.DS_Store")
     deepcopy(list, Yak.BUILD_ROOT)
 
