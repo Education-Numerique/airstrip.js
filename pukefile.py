c2b7c52e07c98a143dfba929f57dffb9df6bf4b2
dmp42 <olivier@webitup.fr>
2012-01-27 13:04:40 +0100

    Fix keyboard url and build dir

--- pukefile.py
+++ pukefile.py
@@ -49,36 +49,36 @@ FILTERING="*-stable.js"
 # Get whatever has been built and exfilter some crappy stuff
 @task("Deploy")
 def deploy():
-    list = FileList(Yak.BUILD_ROOT, exclude = "*.zip,*.tar.gz,*.DS_Store")
+    list = FileList(Yak.TMP_ROOT, exclude = "*.zip,*.tar.gz,*.DS_Store")
     deepcopy(list, Yak.DEPLOY_ROOT)
 
 
 @task("Stats report deploy")
 def stats():
-    list = FileList(Yak.BUILD_ROOT, filter = FILTERING)
+    list = FileList(Yak.TMP_ROOT, filter = FILTERING)
     stats(list, title = "Static statistics - javascript")
-    list = FileList(Yak.BUILD_ROOT, filter = FILTERING.replace(".js", "-min.js"))
+    list = FileList(Yak.TMP_ROOT, filter = FILTERING.replace(".js", "-min.js"))
     stats(list, title = "Static statistics - minified javascript")
-    list = FileList(Yak.BUILD_ROOT, filter = "*-stable.css")
+    list = FileList(Yak.TMP_ROOT, filter = "*-stable.css")
     stats(list, title = "Static statistics - css")
     list = FileList(Yak.DEPLOY_ROOT, filter = "*.html,*.xml,*.txt")
     stats(list, title = "Static statistics - (ht|x)ml + txt")
-    list = FileList(Yak.BUILD_ROOT, exclude = "*.html,*.xml,*.txt,*.js,*.css")
+    list = FileList(Yak.TMP_ROOT, exclude = "*.html,*.xml,*.txt,*.js,*.css")
     stats(list, title = "Static statistics - other")
 
 @task("Linting")
 def lint():
-    list = FileList(Yak.BUILD_ROOT, filter = FILTERING)
+    list = FileList(Yak.TMP_ROOT, filter = FILTERING)
     jslint(list, relax=True)
 
 @task("Flint")
 def flint():
-    list = FileList(Yak.BUILD_ROOT, filter = FILTERING)
+    list = FileList(Yak.TMP_ROOT, filter = FILTERING)
     jslint(list, relax=True, fix=True)
 
 @task("Minting")
 def mint():
-    list = FileList(Yak.BUILD_ROOT, filter = FILTERING)
+    list = FileList(Yak.TMP_ROOT, filter = FILTERING)
     for burne in list.get():
         m = burne.replace(".js", "-min.js")
         minify(burne, m)
@@ -91,14 +91,14 @@ def build():
 
     # Crossdomain
     list = "src/crossdomain.xml"
-    combine(list, Yak.BUILD_ROOT + "/crossdomain.xml", replace = sed)
+    combine(list, Yak.TMP_ROOT + "/crossdomain.xml", replace = sed)
 
     # Robots
     list = "src/robots.txt"
     sed = Sed()
     # XXX partially fucked-up
     sed.add("(?:^|\n+)(?:#[^\n]*\n*)+", "")
-    combine(list, Yak.BUILD_ROOT + "/robots.txt", replace = sed)
+    combine(list, Yak.TMP_ROOT + "/robots.txt", replace = sed)
 
 
 
@@ -112,35 +112,33 @@ def build():
             else:
                 description += "\t\t<dt>" + key + "</dt>\n\t\t<dd>" + "</dd><dd>".join(v) + "</dd>\n"
         description += "\t</dl>\n</section>\n"
-        deepcopy(burne["Source"], os.path.join(Yak.BUILD_ROOT, "lib", burne["Destination"]))
+        deepcopy(burne["Source"], os.path.join(Yak.TMP_ROOT, "lib", burne["Destination"]))
 
-    # h5 = os.path.join(BUILD_ROOT, "lib", fulllist["h5bp-latest"]["Destination"])
+    # h5 = os.path.join(TMP_ROOT, "lib", fulllist["h5bp-latest"]["Destination"])
     # sh("cd " + h5 + "; rm builder.zip; mv builder* builder.zip")
     # unpack(os.path.join(h5, "builder.zip"), h5)
 
 
-    # goog = os.path.join(BUILD_ROOT, "lib", fulllist["closure-stable"]["Destination"])
+    # goog = os.path.join(TMP_ROOT, "lib", fulllist["closure-stable"]["Destination"])
     # unpack(os.path.join(goog, "closure-library-20111110-r1376.zip"), goog)
 
     # Unpack these who need it
-    jasm = os.path.join(Yak.BUILD_ROOT, "lib", Yak.COLLECTION["jasmine-stable.js"]["Destination"])
+    jasm = os.path.join(Yak.TMP_ROOT, "lib", Yak.COLLECTION["jasmine-stable.js"]["Destination"])
     unpack(os.path.join(jasm, "jasmine-standalone-1.1.0.zip"), jasm)
 
-    labjs = os.path.join(Yak.BUILD_ROOT, "lib", Yak.COLLECTION["labjs-stable.js"]["Destination"])
+    labjs = os.path.join(Yak.TMP_ROOT, "lib", Yak.COLLECTION["labjs-stable.js"]["Destination"])
     unpack(os.path.join(labjs, "LABjs-2.0.3.zip"), os.path.join(labjs, "2.0.3"))
 
     for (k, burne) in Yak.COLLECTION.items():
-#        sh("cd " + Yak.BUILD_ROOT + "/lib/" + burne["Destination"] + "; rm " + k + "; ln -s " + burne["Latest"] + " " +  k )
-        sh("cd " + Yak.BUILD_ROOT + "/lib/" + burne["Destination"] + "; cp -R " + burne["Latest"] + " " +  k )
-
+#        sh("cd " + Yak.TMP_ROOT + "/lib/" + burne["Destination"] + "; rm " + k + "; ln -s " + burne["Latest"] + " " +  k )
+        sh("cd " + Yak.TMP_ROOT + "/lib/" + burne["Destination"] + "; cp -R " + burne["Latest"] + " " +  k )
 
     # Build-up the description file
     file = "src/doc.html"
     s = Sed()
     s.add("{PUKE-LIST}", description)
-    deepcopy(file, Yak.BUILD_ROOT, replace=s)
+    deepcopy(file, Yak.TMP_ROOT, replace=s)
 
     # Deepcopy other stuff
-    list = FileList("src", exclude="*robots.txt,*crossdomain.xml,*doc.html,*.DS_Store")
-    deepcopy(list, Yak.BUILD_ROOT)
-
+    list = FileList("src/", exclude="*robots.txt,*crossdomain.xml,*doc.html,*.DS_Store")
+    deepcopy(list, Yak.TMP_ROOT)
